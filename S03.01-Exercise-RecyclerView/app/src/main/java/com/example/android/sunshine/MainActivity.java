12ba7bfe2a75d1841eef41e1c3d6df9acace0448
Intrepid Panda <jerick999@gmail.com>
2019-03-18 18:54:34 -0600

    Set height and width of textview to wrap content

--- S03.01-Exercise-RecyclerView/app/src/main/java/com/example/android/sunshine/MainActivity.java
+++ S03.01-Exercise-RecyclerView/app/src/main/java/com/example/android/sunshine/MainActivity.java
@@ -40,7 +40,7 @@ public class MainActivity extends AppCompatActivity {
 
     // Completed (8) Add a TextView with an id @+id/tv_weather_data
     // Completed (9) Set the text size to 22sp
-    // TODO (10) Make the width and height wrap_content
+    // Completed (10) Make the width and height wrap_content
     // TODO (11) Give the TextView 16dp of padding
 
     // TODO (12) Add a View to the layout with a width of match_parent and a height of 1dp

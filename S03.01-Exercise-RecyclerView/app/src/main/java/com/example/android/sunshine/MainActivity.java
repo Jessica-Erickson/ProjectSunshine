3a63961d18eb12f984190f7537a5ff8935a672ea
Intrepid Panda <jerick999@gmail.com>
2019-03-18 18:59:22 -0600

    Set the left and right margins of the extra view to 8dp

--- S03.01-Exercise-RecyclerView/app/src/main/java/com/example/android/sunshine/MainActivity.java
+++ S03.01-Exercise-RecyclerView/app/src/main/java/com/example/android/sunshine/MainActivity.java
@@ -45,7 +45,7 @@ public class MainActivity extends AppCompatActivity {
 
     // Completed (12) Add a View to the layout with a width of match_parent and a height of 1dp
     // Completed (13) Set the background color to #dadada
-    // TODO (14) Set the left and right margins to 8dp
+    // Completed (14) Set the left and right margins to 8dp
     // Within forecast_list_item.xml //////////////////////////////////////////////////////////////
 
 

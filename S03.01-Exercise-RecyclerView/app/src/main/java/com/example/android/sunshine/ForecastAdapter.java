72fb0943d92a1b601a636dae7c8e439a60ac51e5
Intrepid Panda <jerick999@gmail.com>
2019-03-19 12:48:32 -0600

    Create constructor method for forecastadapterviewholder

--- S03.01-Exercise-RecyclerView/app/src/main/java/com/example/android/sunshine/ForecastAdapter.java
+++ S03.01-Exercise-RecyclerView/app/src/main/java/com/example/android/sunshine/ForecastAdapter.java
@@ -1,10 +1,15 @@
 package com.example.android.sunshine;
 
 import android.support.v7.widget.RecyclerView;
+import android.view.View;
 import android.widget.TextView;
 
 public class ForecastAdapter {
     public class ForecastAdapterViewholder extends RecyclerView.ViewHolder {
         public final TextView mWeatherTextView;
+
+        public ForecastAdapterViewholder(View view) {
+
+        }
     }
 }

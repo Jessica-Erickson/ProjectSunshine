9c02a8336d5ae3ad407917c7b07c687cbeabc8bc
Intrepid Panda <jerick999@gmail.com>
2019-03-19 12:45:55 -0600

    Create member variable in forecastadapterviewholder class called mWeatherTextView

--- S03.01-Exercise-RecyclerView/app/src/main/java/com/example/android/sunshine/ForecastAdapter.java
+++ S03.01-Exercise-RecyclerView/app/src/main/java/com/example/android/sunshine/ForecastAdapter.java
@@ -1,9 +1,10 @@
 package com.example.android.sunshine;
 
 import android.support.v7.widget.RecyclerView;
+import android.widget.TextView;
 
 public class ForecastAdapter {
     public class ForecastAdapterViewholder extends RecyclerView.ViewHolder {
-
+        public final TextView mWeatherTextView;
     }
 }

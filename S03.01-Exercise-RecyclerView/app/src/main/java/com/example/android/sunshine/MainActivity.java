db9ac77c1449bf720cec1f63d8d474eb6a5b6343
Intrepid Panda <jerick999@gmail.com>
2019-03-20 14:27:54 -0600

    Add a private recyclerview member variable called mrecyclerview

--- S03.01-Exercise-RecyclerView/app/src/main/java/com/example/android/sunshine/MainActivity.java
+++ S03.01-Exercise-RecyclerView/app/src/main/java/com/example/android/sunshine/MainActivity.java
@@ -18,6 +18,7 @@ package com.example.android.sunshine;
 import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.RecyclerView;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
@@ -86,8 +87,10 @@ public class MainActivity extends AppCompatActivity {
 
     // Completed (33) Delete mWeatherTextView
 
-    // TODO (34) Add a private RecyclerView variable called mRecyclerView
+    // Completed (34) Add a private RecyclerView variable called mRecyclerView
     // TODO (35) Add a private ForecastAdapter variable called mForecastAdapter
+
+    private RecyclerView mRecyclerView;
 
     private TextView mErrorMessageDisplay;
 

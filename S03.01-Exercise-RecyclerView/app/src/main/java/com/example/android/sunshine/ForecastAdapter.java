b611292268566f1f301763452b317a6dcc0d5968
Intrepid Panda <jerick999@gmail.com>
2019-03-20 14:05:10 -0600

    Override oncreateviewholder in forecastadapter class

--- S03.01-Exercise-RecyclerView/app/src/main/java/com/example/android/sunshine/ForecastAdapter.java
+++ S03.01-Exercise-RecyclerView/app/src/main/java/com/example/android/sunshine/ForecastAdapter.java
@@ -1,7 +1,9 @@
 package com.example.android.sunshine;
 
+import android.support.annotation.NonNull;
 import android.support.v7.widget.RecyclerView;
 import android.view.View;
+import android.view.ViewGroup;
 import android.widget.TextView;
 
 public class ForecastAdapter extends RecyclerView.Adapter<ForecastAdapter.ForecastAdapterViewholder> {
@@ -15,5 +17,11 @@ public class ForecastAdapter extends RecyclerView.Adapter<ForecastAdapter.Foreca
 
             mWeatherTextView = (TextView) view.findViewById(R.id.tv_weather_data);
         }
+    }
+
+    @NonNull
+    @Override
+    public ForecastAdapterViewholder onCreateViewHolder(@NonNull ViewGroup viewGroup, int i) {
+        return null;
     }
 }

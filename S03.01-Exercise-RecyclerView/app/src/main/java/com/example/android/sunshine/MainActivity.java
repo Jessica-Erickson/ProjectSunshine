75b80c3b19bab694abc2059d8e2c5d58450afaa6
Intrepid Panda <jerick999@gmail.com>
2019-03-20 14:46:55 -0600

    Set recyclerview's adapter to mforecast adapter

--- S03.01-Exercise-RecyclerView/app/src/main/java/com/example/android/sunshine/MainActivity.java
+++ S03.01-Exercise-RecyclerView/app/src/main/java/com/example/android/sunshine/MainActivity.java
@@ -128,7 +128,8 @@ public class MainActivity extends AppCompatActivity {
         // Completed (41) set mForecastAdapter equal to a new ForecastAdapter
         mForecastAdapter = new ForecastAdapter();
 
-        // TODO (42) Use mRecyclerView.setAdapter and pass in mForecastAdapter
+        // Completed (42) Use mRecyclerView.setAdapter and pass in mForecastAdapter
+        mRecyclerView.setAdapter(mForecastAdapter);
 
         /*
          * The ProgressBar that will indicate to the user that we are loading data. It will be

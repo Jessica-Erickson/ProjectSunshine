2d737275ca383970f5e3a637101491b03228ce9e
Intrepid Panda <jerick999@gmail.com>
2019-03-20 14:09:03 -0600

    Inflate list item into a view in oncreateviewholder

--- S03.01-Exercise-RecyclerView/app/src/main/java/com/example/android/sunshine/ForecastAdapter.java
+++ S03.01-Exercise-RecyclerView/app/src/main/java/com/example/android/sunshine/ForecastAdapter.java
@@ -1,7 +1,9 @@
 package com.example.android.sunshine;
 
+import android.content.Context;
 import android.support.annotation.NonNull;
 import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextView;
@@ -22,6 +24,12 @@ public class ForecastAdapter extends RecyclerView.Adapter<ForecastAdapter.Foreca
     @NonNull
     @Override
     public ForecastAdapterViewholder onCreateViewHolder(@NonNull ViewGroup viewGroup, int i) {
+        Context context = viewGroup.getContext();
+        int listItemId = R.layout.forecast_list_item;
+        LayoutInflater inflater = LayoutInflater.from(context);
+        boolean shouldAttachImmediately = false;
+        View view = inflater.inflate(listItemId, viewGroup, shouldAttachImmediately);
+
         return null;
     }
 }

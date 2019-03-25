1bd3c179ff8c29d67eef015aeadec09ffe2f9d3e
Intrepid Panda <jerick999@gmail.com>
2019-03-25 06:17:53 -0600

    Replace the toast notification in onclick method with and explicit intent that opens detail activity

--- S04.01-Exercise-LaunchNewActivity/app/src/main/java/com/example/android/sunshine/MainActivity.java
+++ S04.01-Exercise-LaunchNewActivity/app/src/main/java/com/example/android/sunshine/MainActivity.java
@@ -16,6 +16,7 @@
 package com.example.android.sunshine;
 
 import android.content.Context;
+import android.content.Intent;
 import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
@@ -119,9 +120,10 @@ public class MainActivity extends AppCompatActivity implements ForecastAdapterOn
         Context context = this;
         // Completed (1) Create a new Activity called DetailActivity using Android Studio's wizard
         // Completed (2) Change the root layout of activity_detail.xml to a FrameLayout and remove unnecessary xml attributes
-        // TODO (3) Remove the Toast and launch the DetailActivity using an explicit Intent
-        Toast.makeText(context, weatherForDay, Toast.LENGTH_SHORT)
-                .show();
+        // Completed (3) Remove the Toast and launch the DetailActivity using an explicit Intent
+        Class destination = DetailActivity.class;
+        Intent intent = new Intent(context, destination);
+        startActivity(intent);
     }
 
     /**

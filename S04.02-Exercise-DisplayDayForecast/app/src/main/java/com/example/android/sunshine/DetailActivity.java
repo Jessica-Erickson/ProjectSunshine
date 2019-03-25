32d2142b3837ac8a6a9ae62e5dc1fc5647973c03
Intrepid Panda <jerick999@gmail.com>
2019-03-25 06:33:34 -0600

    Verify that the initializing intent is not null and has extra text, then set that text to the detail weather display

--- S04.02-Exercise-DisplayDayForecast/app/src/main/java/com/example/android/sunshine/DetailActivity.java
+++ S04.02-Exercise-DisplayDayForecast/app/src/main/java/com/example/android/sunshine/DetailActivity.java
@@ -1,5 +1,6 @@
 package com.example.android.sunshine;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.widget.TextView;
@@ -18,7 +19,13 @@ public class DetailActivity extends AppCompatActivity {
 
         mWeatherDisplay = (TextView) findViewById(R.id.tv_detail_display);
 
+        Intent initializer = getIntent();
 
-        // TODO (2) Display the weather forecast that was passed from MainActivity
+        if (initializer != null && initializer.hasExtra(Intent.EXTRA_TEXT)) {
+            mWeatherDetail = initializer.getStringExtra(Intent.EXTRA_TEXT);
+            mWeatherDisplay.setText(mWeatherDetail);
+        }
+
+        // Completed (2) Display the weather forecast that was passed from MainActivity
     }
 }
